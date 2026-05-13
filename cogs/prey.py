c7e84f3783bc176c473ae02da3c7b1b958658049
Amanda Viers <amandaviers@Amandas-MacBook-Pro.local>
2026-05-12 20:20:47 -0500

    decrease prey chance

--- cogs/prey.py
+++ cogs/prey.py
@@ -10,7 +10,7 @@ logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', level=lo
 # [optional arg]
 command_guide = "!prey <area> <skill> <attempts>"
 hibernate = False
-global_modifier = 5
+global_modifier = 8
 prey_scarcity = 15
 
 error_embed = discord.Embed(title="<:error:1492739840230428829> Error", description="Error", color=discord.Color.red())
