3063739b199e84cefaccffab7b990d98c7790d00
Amanda Viers <amandaviers@Amandas-MacBook-Pro.local>
2026-06-18 19:21:49 -0500

    fix probabilties

--- cogs/prey.py
+++ cogs/prey.py
@@ -11,8 +11,8 @@ logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', level=lo
 # [optional arg]
 command_guide = "!prey <area> <skill> <attempts>"
 hibernate = False
-global_modifier = 5
-prey_scarcity = 8
+global_modifier = 6
+prey_scarcity = 10
 
 error_embed = discord.Embed(title="<:error:1492739840230428829> Error", description="Error", color=discord.Color.red())
 
