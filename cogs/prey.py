e27e2674c708780a9294e727a23708eb18e1fbfb
Amanda Viers <amandaviers@Amandas-MacBook-Pro.local>
2026-06-18 19:25:29 -0500

    fix

--- cogs/prey.py
+++ cogs/prey.py
@@ -11,8 +11,8 @@ logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', level=lo
 # [optional arg]
 command_guide = "!prey <area> <skill> <attempts>"
 hibernate = False
-global_modifier = 6
-prey_scarcity = 20
+global_modifier = 8
+prey_scarcity = 10
 
 error_embed = discord.Embed(title="<:error:1492739840230428829> Error", description="Error", color=discord.Color.red())
 
